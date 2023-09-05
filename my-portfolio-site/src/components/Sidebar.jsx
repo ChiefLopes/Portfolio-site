a0e9bbc14c59419f43b355d31c468f8cc95262b8
ChiefLopes <chieflopesekede@gmail.com>
2023-09-05 14:01:54 +0100

    added fontawesome icons to the sidebar, with edits to it

--- my-portfolio-site/src/components/Sidebar.jsx
+++ my-portfolio-site/src/components/Sidebar.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import { Link, NavLink} from "react-router-dom";
 import image from "../assests/images/images (1).png";
-import { fontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons';
 
 
 const SideBar = () => {
@@ -13,7 +13,15 @@ const SideBar = () => {
         <h2 className="logo-title">ChiefLopes</h2>
       </Link>
       <nav>
-        <NavLink exact="true" activeclassname="active" to='/'></NavLink>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} size="2x" color="#8d3d3e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faUser} size="2x" color="#8d3d3e" />
+        </NavLink>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faEnvelope} size="2x" color="#8d3d3e" />
+        </NavLink>
       </nav>
     </div>
   );
