dba74d519b1aa9284f6464638848fb72182f4a34
ChiefLopes <chieflopesekede@gmail.com>
2023-09-05 13:27:27 +0100

    added fontawesome icons

--- my-portfolio-site/src/components/Sidebar.jsx
+++ my-portfolio-site/src/components/Sidebar.jsx
@@ -1,17 +1,20 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import image from '../assests/images/images (1).png'
-
+import { Link, NavLink} from "react-router-dom";
+import image from "../assests/images/images (1).png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const SideBar = () => {
-    return (
-      <div className="nav-bar">
-        <Link>
-                <img className="sideBar-image" src={image} alt="" />
-                <h2 className="logo-title">ChiefLopes</h2>
-        </Link>
-      </div>
-    );
-}
+  return (
+    <div className="nav-bar">
+      <Link>
+        <img className="sideBar-image" src={image} alt="" />
+        <h2 className="logo-title">ChiefLopes</h2>
+      </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to='/'></NavLink>
+      </nav>
+    </div>
+  );
+};
 
 export default SideBar;
