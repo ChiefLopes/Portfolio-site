fec2d09eb2020541bf92b715ba735907d2386ed5
ChiefLopes <chieflopesekede@gmail.com>
2023-09-06 15:28:16 +0100

    added new features

--- my-portfolio-site/src/components/Sidebar/index.jsx
+++ my-portfolio-site/src/components/Sidebar/index.jsx
@@ -1,15 +1,36 @@
-import React from "react"
+import React from "react";
 import "./index.scss";
-import { Link } from "react-router-dom";
-import logo from '../../assets/images/dice.jpg'
+import { Link, NavLink } from "react-router-dom";
+import logo from "../../assets/images/Lunix.png";
+import { FontAwesome, FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
   return (
     <div className="nav-bar">
       <Link className="logo" to="/">
-              <img className="sideBar-image" src={logo} alt="" />
-              <h2>Mufasa.Don</h2>
+        <img src={logo} alt="" />
+        <h2 className="slogan">Lunix</h2>
       </Link>
+      <nav>
+        <NavLink exact="true" activeclassname="active" to="/">
+          <FontAwesomeIcon icon={faHome} color="#4d4d3e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/">
+          <FontAwesomeIcon icon={faUser} color="#4d4d3e" />
+        </NavLink>
+        <NavLink
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/">
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d3e" />
+        </NavLink>
+      </nav>
     </div>
   );
 };
