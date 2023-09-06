fdf11813cfb60527fdfb71e4fc7db118cd8feb90
ChiefLopes <chieflopesekede@gmail.com>
2023-09-06 12:23:20 +0100

    successfully added saas

--- my-portfolio-site/src/App.js
+++ my-portfolio-site/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./App.css";
 import { Route, Router, Routes } from "react-router-dom";
-// import Layout from './components/Layout';
+import Layout from './components/Layout';
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="" element="" />
+        <Route path="" element={<Layout />} />
       </Routes>
     </div>
   );
---
+++ my-portfolio-site/src/components/Layout/index.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import './index.scss'
+import Sidebar from "../Sidebar";
+
+
+
+const Layout = () => {
+    return (
+        <>
+           <Sidebar />
+        </>
+        )
+}
+
+export default Layout;
