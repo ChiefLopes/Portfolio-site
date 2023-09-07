e7bdf9de1d5e32395a8beedc11cc1ef5fdb690bd
ChiefLopes <chieflopesekede@gmail.com>
2023-09-07 13:02:19 +0100

    added new features

--- my-portfolio-site/src/components/Sidebar/index.jsx
+++ my-portfolio-site/src/components/Sidebar/index.jsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./index.scss";
-import { Link, NavLink, Router} from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import logo from "../../assets/images/Lunix.png";
-import logotxt from '../../assets/images/Lunixslo.png'
-import { FontAwesome, FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faEnvelope, faHome, faUser } from "@fortawesome/free-solid-svg-icons";
+import logotxt from "../../assets/images/Lunixslo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faEnvelope,
+  faHome,
+  faUser,
+  faLink,
+  faSms,
+  faX,
+  faFaceMeh,
+} from "@fortawesome/free-solid-svg-icons";
 
 const Sidebar = () => {
   return (
@@ -32,6 +40,40 @@ const Sidebar = () => {
           <FontAwesomeIcon icon={faEnvelope} color="#4d4d3e" />
         </NavLink>
       </nav>
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.facebook.com/ChiefLopes">
+            <FontAwesomeIcon icon={faFaceMeh} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="linkedin.com/in/louis-peace-75631925b">
+            <FontAwesomeIcon icon={faLink} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.github.com/ChiefLopes">
+            <FontAwesomeIcon icon={faSms} color="#4d4d4e" />
+          </a>
+        </li>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.twitter.com/ChiefLopes">
+            <FontAwesomeIcon icon={faX} color="#4d4d4e" />
+          </a>
+        </li>
+      </ul>
     </div>
   );
 };
