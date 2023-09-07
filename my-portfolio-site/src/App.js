3072edefa2ec8363c183e97fd9f5fa605716c9da
ChiefLopes <chieflopesekede@gmail.com>
2023-09-07 14:22:36 +0100

    added an Home page and other feautures

--- my-portfolio-site/src/App.js
+++ my-portfolio-site/src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import "./App.css";
-import { Route, Router, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Layout from './components/Layout';
+import Home from "./components/Home";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        <Route path="" element={<Layout />} />
+              <Route path="" element={<Layout />} />
+              <Route index element={<Home /> } />
       </Routes>
     </div>
   );
