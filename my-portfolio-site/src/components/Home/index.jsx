9523255ef64e011e3e77f384e7100fae437b378f
ChiefLopes <chieflopesekede@gmail.com>
2023-09-18 15:06:06 +0100

    added the Home component

--- my-portfolio-site/src/components/Home/index.jsx
+++ my-portfolio-site/src/components/Home/index.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Home = () => {
     return (
         <div className="container home-page">
-            
+            <div className="text-zone">
+                <h1>Hi, <br /> I'm </h1>
+                <h1>Louis Peace</h1>
+            </div>
             
        </div>
         )
